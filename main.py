7335f29fa07ba871a2292d5322368ae322157a2c
zhyncs <me@zhyncs.com>
2022-11-18 00:06:55 +0800

    fix total balance

--- main.py
+++ main.py
@@ -7,16 +7,20 @@ from binance.um_futures import UMFutures
 
 
 def get_sh_time() -> str:
-    sh_tz = datetime.timezone(datetime.timedelta(hours=8),
-                              name="Asia/Shanghai")
-    return (datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).
-            astimezone(sh_tz).strftime("%Y-%m-%d %H:%M:%S") + "\n")
+    sh_tz = datetime.timezone(datetime.timedelta(hours=8), name="Asia/Shanghai")
+    return (
+        datetime.datetime.utcnow()
+        .replace(tzinfo=datetime.timezone.utc)
+        .astimezone(sh_tz)
+        .strftime("%Y-%m-%d %H:%M:%S")
+        + "\n"
+    )
 
 
 def get_total_balance(um_futures_client: UMFutures) -> str:
     total_balance = 0.0
     for ele in um_futures_client.balance():
-        total_balance += float(ele.get("maxWithdrawAmount"))
+        total_balance += float(ele.get("crossWalletBalance"))
     return "totalAccountValue={}\n\n".format(round(total_balance, 3))
 
 
@@ -31,17 +35,28 @@ def get_change_percent(title: str, num: float) -> str:
 
 
 def get_token_info(cm_futures_client: CMFutures, token_list: str) -> str:
+    symbol_to_pair_percent = dict()
+    for ele in cm_futures_client.ticker_24hr_price_change():
+        symbol_to_pair_percent.update(
+            {
+                ele.get("symbol"): (
+                    ele.get("pair"),
+                    ele.get("priceChangePercent"),
+                    ele.get("lastPrice"),
+                )
+            }
+        )
     res = ""
     for token in token_list:
-        for ele in cm_futures_client.ticker_24hr_price_change():
-            if ele.get("symbol") == token + "USD_PERP":
-                bold_pair_str = "*" + ele.get("pair") + "*"
-                change24h_str = get_change_percent(
-                    "change24h", float(ele.get("priceChangePercent")))
-                tmp = "{}\nlast={}\n{}\n\n".format(bold_pair_str,
-                                                   ele.get("lastPrice"),
-                                                   change24h_str)
-                res += tmp
+        symbol = token + "USD_PERP"
+        if symbol in symbol_to_pair_percent:
+            pair_percent = symbol_to_pair_percent.get(symbol)
+            bold_pair_str = "*" + pair_percent[0] + "*"
+            change24h_str = get_change_percent("change24h", float(pair_percent[1]))
+            tmp = "{}\nlast={}\n{}\n\n".format(
+                bold_pair_str, pair_percent[2], change24h_str
+            )
+            res += tmp
 
     return res.strip()
 
